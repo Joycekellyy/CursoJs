8d8033cee32118f08512e2a5566ce810476ac09f
Joyce Kelly <63926941+Joycekellyy@users.noreply.github.com>
2020-04-28 17:38:56 -0300

    Add files via upload

---
+++ Aulas/AULA - 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 17
+console.log(`A velocidade do seu automóvel é ${vel} Km/h.`)
+if(vel > 60) {//CONDIÇÃO SIMPLES
+    console.log('Você foi MULTADO por exesso de velocidade!')
+}
+console.log('Dirija sempre com cinto de segurança! ')
