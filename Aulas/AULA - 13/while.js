61fc000d7c61f0f2349a9ee1030caf71e0e43ad6
Joyce Kelly <63926941+Joycekellyy@users.noreply.github.com>
2020-05-05 17:06:10 -0300

    Add files via upload

---
+++ Aulas/AULA - 13/while.js
@@ -0,0 +1,13 @@
+var cont = 1
+
+while (cont <= 6) {//ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO
+    console.log(`Teste lógico no início = ${cont}`)
+    cont++
+}
+
+cont = 1
+
+do {//ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL
+    console.log(`Teste lógico no final = ${cont}`)
+    cont++
+}while (cont <= 6)
